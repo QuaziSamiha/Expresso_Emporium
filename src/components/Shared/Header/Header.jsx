2e625991ce76a64689f398e69fbfb96a12057837
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-02-26 10:37:30 +0600

    heaeder done

--- src/components/Shared/Header/Header.jsx
+++ src/components/Shared/Header/Header.jsx
@@ -4,13 +4,15 @@ const Header = () => {
   return (
     <>
       <section>
-        <div className="bg-[#372727] h-16 w-full">
-          <div className="flex">
-            <div>
-              <img src={logo} alt="" />
-            </div>
-            <div className="border border-red-500 w-80 h-32">
-              <img src={title} alt="" />
+        <div className="bg-[#372727] h-14">
+          <div className="flex justify-center items-center">
+            <div className="flex my-4 lg:my-2">
+              <div>
+                <img src={logo} alt="" className="h-8 lg:h-10 mr-1" />
+              </div>
+              <div className="">
+                <img src={title} alt="" className="h-8 lg:h-10 ml-1" />
+              </div>
             </div>
           </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import router from "./routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <div className="">
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>
 );
