fd6903acd90abd41e0a709297b87e10f4d3fb91c
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-02-26 11:17:19 +0600

    demo banner added

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,10 @@
+import banner from "../../../assets/images/404/banner.png";
+const Banner = () => {
+  return (
+    <>
+      <img src={banner} alt="" />
+    </>
+  );
+};
+
+export default Banner;
